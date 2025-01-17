ba88c49abdfb0ab8b53590fd2abde3b12eef7c26
Yeamelack <yeamelack@Yeamelacks-MacBook-Air.local>
2025-01-17 14:13:34 -0600

    Math Methods

---
+++ Math Methods/js/main.js
@@ -0,0 +1,32 @@
+
+console.log(Math.PI);
+
+
+//Math.trunc(): remove the integers after the decimal returning a whole number 
+console.log(Math.trunc(Math.PI));
+
+//rounds a number to the nearest whole number
+console.log(Math.round(Math.PI)); // returns 3
+
+//Math.ceil(): rounds any number up
+console.log(Math.ceil(3.1)); // returns 4
+
+//Math.floor(): rounds numbers down regaurdless of the decimal 
+console.log(Math.floor(3.9)); // returns 3
+
+//Math.pow(x,y): returns x^y
+console.log(Math.pow(2,3)) // retruns 8
+
+//Math.min(x, y , z , .. , n): returns the min
+console.log(Math.min(2,3,4,5,6)); // return 2
+
+//Math.Max(x, y, z , .. , n): returns the max
+console.log(Math.max(2,3,4,5,6)); // return 6
+
+//Math.random(): provides a random number between 0-1 not including 1
+console.log(Math.random()); 
+
+
+
+
+
