d23ceb3b297992abd8da59a9ddb5a40a7c00d0d2
Yeamelack <yeamelack@Yeamelacks-MacBook-Air.local>
2025-01-19 16:31:58 -0600

    User Inputs

---
+++ User inputs/js/main.js
@@ -0,0 +1,16 @@
+//alert("Hello World");
+
+
+//BANNER POPUP
+let myBoolean = confirm("OK === True\n cancel == = false");
+console.log(myBoolean);
+
+//returns NULL when cancelled or false
+let userName = prompt("Please enter your name.");
+
+//false if zero
+if(userName){
+    console.log(userName);
+}else{
+    console.log("you didnt enter your name")
+}
